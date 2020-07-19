9c497156c84ab7bd002a35e24096c7851936e120
mariiavolkova <67053060+mariiavolkova@users.noreply.github.com>
2020-07-19 18:40:27 +0300

    Update ValuesList.java

--- src/main/java/com/volkova/valueslist/ValuesList.java
+++ src/main/java/com/volkova/valueslist/ValuesList.java
@@ -3,10 +3,10 @@ package main.java.com.volkova.valueslist;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ValuesList<integerList> {
+public class ValuesList<T> {
     List<Integer> integerList = new ArrayList<>();
 
-    public void addInt(){
+    public void addInt() {
         integerList.add(3);
         integerList.add(9);
         integerList.add(11);
@@ -14,9 +14,10 @@ public class ValuesList<integerList> {
         integerList.add(20);
         integerList.add(22);
     }
-    public void deleteInteger(){
-    integerList.removeIf(integer -> integer % 3 == 0);
-    System.out.println("This Numbers not divide by 3: ");
-    integerList.forEach(integer -> System.out.println(integer + "   "));
-}
+
+    public void deleteInteger() {
+        integerList.removeIf(integer -> integer % 3 == 0);
+        System.out.println("This Numbers not divide by 3: ");
+        integerList.forEach(integer -> System.out.println(integer + "   "));
+    }
 }
